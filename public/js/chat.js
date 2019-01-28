976ee8d8c525039f60ec348095cedfb2c123b52f
Tomasz Wylężek <tomwylezek@gmail.com>
2019-01-28 18:01:30 +0100

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,117 +1,100 @@
 const socket = io();
 
-function scrollToBottom() {
+function scrollToBottom () {
   // Selectors
-  const messages = $("#messages");
-  const newMessage = messages.children("li:last-child");
+  const messages = $('#messages');
+  const newMessage = messages.children('li:last-child')
   // Heights
-  const clientHeight = messages.prop("clientHeight");
-  const scrollTop = messages.prop("scrollTop");
-  const scrollHeight = messages.prop("scrollHeight");
+  const clientHeight = messages.prop('clientHeight');
+  const scrollTop = messages.prop('scrollTop');
+  const scrollHeight = messages.prop('scrollHeight');
   const newMessageHeight = newMessage.innerHeight();
   const lastMessageHeight = newMessage.prev().innerHeight();
 
-  if (
-    clientHeight + scrollTop + newMessageHeight + lastMessageHeight >=
-    scrollHeight
-  ) {
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
     messages.scrollTop(scrollHeight);
   }
 }
 
-socket.on("connect", function() {
-  console.log("Connected to server");
+socket.on('connect', function () {
   const params = $.deparam(window.location.search);
 
-  socket.emit("join", params, function(err) {
+  socket.emit('join', params, function (err) {
     if (err) {
       alert(err);
-      window.location.href = "/";
-    } else {
+      window.location.href = '/';
     }
   });
 });
 
-socket.on("disconnect", function() {
-  console.log("Disconnected from server");
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
 });
 
-socket.on("updateUserList", function(users) {
-  console.log("User list", users);
+socket.on('updateUserList', function (users) {
+  const ol = $('<ol></ol>');
 
-  const ol = $("<ol></ol>");
-
-  users.forEach(function(user) {
-    ol.append($("<li></li>").text(user));
+  users.forEach(function (user) {
+    ol.append($('<li></li>').text(user));
   });
 
-  $("#users").html(ol);
+  $('#users').html(ol);
 });
 
-socket.on("newMessage", function(message) {
-  const formattedTime = moment(message.createdAt).format("h:mm a");
-  const template = $("#message-template").html();
-
+socket.on('newMessage', function (message) {
+  const formattedTime = moment(message.createdAt).format('h:mm a');
+  const template = $('#message-template').html();
   const html = Mustache.render(template, {
     text: message.text,
     from: message.from,
     createdAt: formattedTime
   });
 
-  $("#messages").append(html);
+  $('#messages').append(html);
   scrollToBottom();
 });
 
-socket.on("newLocationMessage", function(message) {
-  const formattedTime = moment(message.createdAt).format("h:mm a");
-  const template = $("#location-message-template").html();
+socket.on('newLocationMessage', function (message) {
+  const formattedTime = moment(message.createdAt).format('h:mm a');
+  const template = $('#location-message-template').html();
   const html = Mustache.render(template, {
-    url: message.url,
     from: message.from,
+    url: message.url,
     createdAt: formattedTime
   });
 
-  $("#messages").append(html);
+  $('#messages').append(html);
   scrollToBottom();
 });
 
-jQuery("#message-form").on("submit", function(e) {
+$('#message-form').on('submit', function (e) {
   e.preventDefault();
 
-  const messageTextbox = jQuery("[name=message]");
+  const messageTextbox = $('[name=message]');
 
-  socket.emit(
-    "createMessage",
-    {
-      from: "User",
-      text: messageTextbox.val()
-    },
-    function() {
-      messageTextbox.val("");
-    }
-  );
+  socket.emit('createMessage', {
+    text: messageTextbox.val()
+  }, function () {
+    messageTextbox.val('')
+  });
 });
 
-const locationButton = jQuery("#send-location");
-
-locationButton.on("click", function() {
+const locationButton = $('#send-location');
+locationButton.on('click', function () {
   if (!navigator.geolocation) {
-    return alert("Geolocation not supported by your browser.");
+    return alert('Geolocation not supported by your browser.');
   }
 
-  locationButton.attr("disabled", "disabled").text("Sending location...");
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
 
-  navigator.geolocation.getCurrentPosition(
-    function(position) {
-      locationButton.removeAttr("disabled").text("Send location");
-      socket.emit("createLocationMessage", {
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude
-      });
-    },
-    function() {
-      locationButton.removeAttr("disabled").text("Send location");
-      alert("Unable to fetch location.");
-    }
-  );
+  navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
+    alert('Unable to fetch location.');
+  });
 });
--- server/server.js
+++ server/server.js
@@ -2,17 +2,16 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketIO = require("socket.io");
+
 const { generateMessage, generateLocationMessage } = require("./utils/message");
 const { isRealString } = require("./utils/validation");
 const { Users } = require("./utils/users");
 
+const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, "../public/");
 const app = express();
-
 const server = http.createServer(app);
 const io = socketIO(server);
-
 const users = new Users();
 
 app.use(express.static(publicPath));
@@ -26,7 +25,6 @@ io.on("connection", socket => {
     }
 
     socket.join(params.room);
-
     users.removeUser(socket.id);
     users.addUser(socket.id, params.name, params.room);
 
@@ -35,28 +33,36 @@ io.on("connection", socket => {
       "newMessage",
       generateMessage("Admin", "Welcome to the chat app")
     );
-
     socket.broadcast
       .to(params.room)
       .emit(
         "newMessage",
-        generateMessage("Admin", `${params.name} has joined`)
+        generateMessage("Admin", `${params.name} has joined.`)
       );
-    callback();
   });
 
   socket.on("createMessage", (message, callback) => {
-    console.log("createMessage", message);
-    io.emit("newMessage", generateMessage(message.from, message.text));
+    const user = users.getUser(socket.id);
+
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit(
+        "newMessage",
+        generateMessage(user.name, message.text)
+      );
+    }
 
     callback();
   });
 
   socket.on("createLocationMessage", coords => {
-    io.emit(
-      "newLocationMessage",
-      generateLocationMessage("Admin", coords.latitude, coords.longitude)
-    );
+    const user = users.getUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "newLocationMessage",
+        generateLocationMessage(user.name, coords.latitude, coords.longitude)
+      );
+    }
   });
 
   socket.on("disconnect", () => {
@@ -66,12 +72,12 @@ io.on("connection", socket => {
       io.to(user.room).emit("updateUserList", users.getUserList(user.room));
       io.to(user.room).emit(
         "newMessage",
-        generateMessage("Admin", `${user.name} has left`)
+        generateMessage("Admin", `${user.name} has left.`)
       );
     }
   });
 });
 
 server.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
+  console.log(`Server is up on ${port}`);
 });

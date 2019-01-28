d002fe6509e17d7ef7efcf434890519849950ec1
Tomasz Wylężek <tomwylezek@gmail.com>
2019-01-28 15:44:23 +0100

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,24 @@
 const socket = io();
 
+function scrollToBottom() {
+  // Selectors
+  const messages = $("#messages");
+  const newMessage = messages.children("li:last-child");
+  // Heights
+  const clientHeight = messages.prop("clientHeight");
+  const scrollTop = messages.prop("scrollTop");
+  const scrollHeight = messages.prop("scrollHeight");
+  const newMessageHeight = newMessage.innerHeight();
+  const lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (
+    clientHeight + scrollTop + newMessageHeight + lastMessageHeight >=
+    scrollHeight
+  ) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on("connect", function() {
   console.log("Connected to server");
 });
@@ -19,6 +38,7 @@ socket.on("newMessage", function(message) {
   });
 
   $("#messages").append(html);
+  scrollToBottom();
 });
 
 socket.on("newLocationMessage", function(message) {
@@ -31,6 +51,7 @@ socket.on("newLocationMessage", function(message) {
   });
 
   $("#messages").append(html);
+  scrollToBottom();
 });
 
 jQuery("#message-form").on("submit", function(e) {

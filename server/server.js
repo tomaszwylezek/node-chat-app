3128db0ae38ff1b4993d8e2c89fc1082f43ab1a0
Tomasz Wylężek <tomwylezek@gmail.com>
2019-01-28 14:17:07 +0100

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,18 @@ app.use(express.static(publicPath));
 io.on("connection", socket => {
   console.log("New user connected");
 
+  socket.emit("newMessage", {
+    from: "Admin",
+    text: "Welcome to the chat app",
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit("newMessage", {
+    from: "Admin",
+    text: "New user joined",
+    createdAt: new Date().getTime()
+  });
+
   socket.on("disconnect", () => {
     console.log("User was disconnected");
   });
@@ -26,6 +38,12 @@ io.on("connection", socket => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+
+    // socket.broadcast.emit("newMessage", {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 });
 
